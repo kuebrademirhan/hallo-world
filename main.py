cef1b6740e0896cfddf92f34b324ab514305bb52
hopeful12 <93667932+hopeful12@users.noreply.github.com>
2021-11-04 17:45:40 +0100

    function added

--- main.py
+++ main.py
@@ -13,4 +13,10 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
 
+
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def print_hallowelt():
+    print("Hallo Welt")
+
+
+print_hallowelt()
