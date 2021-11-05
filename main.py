3281e0b5c3391494dbe221095695f02e3241ac89
chris <christian.schlieker@a-b-f.de>
2021-11-05 14:52:00 +0100

    UI generated with QTdesigner added. https://pythonpyqt.com/how-to-install-pyqt5-in-pycharm/

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+import sys
+from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMainWindow
 # This is a sample Python script.
 
 # Press Umschalt+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import untitled
+
+
+class testGUI(untitled.Ui_MainWindow, QMainWindow):
+
+    def __init__(self):
+        super(testGUI, self).__init__()
+        self.setupUi(self)
 
 
 def print_hi(name):
@@ -9,9 +20,27 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def qt_test():
+    # Create the application object
+    app = QtWidgets.QApplication(sys.argv)
+
+    # directly create the form object
+    # first_window = QtWidgets.QWidget()
+    # Create window from UI defined in untitled.py
+    first_window = testGUI()
+
+    # Set window size
+    #first_window.resize(400, 300)
+
+    # Set the form title
+    #first_window.setWindowTitle("The first pyqt program")
+
+    # Show form
+    first_window.show()
+
+    # Run the program
+    sys.exit(app.exec())
+
 
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
@@ -19,4 +48,10 @@ def print_hallowelt():
     print("Hallo Welt")
 
 
-print_hallowelt()
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
+    print_hallowelt()
+    qt_test()
+
+
